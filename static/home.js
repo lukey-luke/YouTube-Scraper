f64b42a673e127463f1ae0f11df05474054af6ae
Luke Short <lshort2@mail.csuchico.edu>
2019-04-28 19:06:44 -0700

    display subscriber counts at bottom of page

--- static/home.js
+++ static/home.js
@@ -2,11 +2,16 @@ function draw_winnername_logo(username) {}
 function get_sub_counts() {
     return $.get('/api/get_sub_counts')
 }
+function draw_sub_counts(tseries_subscribers, pewdiepie_subscribers) {
+    $('#tseries_subscribers_count').html(tseries_subscribers);
+    $('#pewdiepie_subscribers_count').html(pewdiepie_subscribers);
+}
 
 
 $(document).ready(function() {
     get_sub_counts().then(function(data) {
         create_chart(data["tseries"], data["pewdiepie"]);
+        draw_sub_counts(data["tseries"], data["pewdiepie"]);
         return data["pewdiepie"] > data["tseries"];
 
     }).then(function(isPewdiepieWinner) {
